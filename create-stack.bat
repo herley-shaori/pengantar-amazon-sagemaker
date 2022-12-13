e274360a7b6eafdf34b9ccd40e6e350018cf9a3d
herley <herley@gmail.com>
2022-12-13 13:16:51 +0700

    updated cloudformation bat.

--- create-stack.bat
+++ create-stack.bat
@@ -1,1 +1,6 @@
 aws cloudformation create-stack --stack-name jhbasdhjasd --template-body file://notebook-instance.yaml --capabilities CAPABILITY_NAMED_IAM
+timeout 30
+aws s3api put-object --bucket pengantar-sagemaker --key train/
+aws s3api put-object --bucket pengantar-sagemaker --key test/
+aws s3api put-object --bucket pengantar-sagemaker --key validate/
+aws s3api put-object --bucket pengantar-sagemaker --key model/  
--- delete-stack.bat
+++ delete-stack.bat
@@ -1,1 +1,3 @@
+aws s3 rm s3://pengantar-sagemaker --recursive
+aws s3api delete-bucket --bucket pengantar-sagemaker
 aws cloudformation delete-stack --stack-name jhbasdhjasd
